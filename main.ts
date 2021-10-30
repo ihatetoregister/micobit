8a38a357051fd2daa3664737c60aebdc78c2f784
ihatetoregister <ihatetoregister@users.noreply.github.com>
2021-10-30 17:49:27 +0200

    Uppdatera main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 function triangle (x: number) {
     return Math.abs(x % (1023 * 2) - 1023)
 }
+let blinkPaus = 0
 let sannolikhet = 0
 let x = 0
 basic.showLeds(`
@@ -18,16 +19,20 @@ basic.forever(function () {
     if (Math.randomBoolean()) {
         sannolikhet = randint(0, 100)
         if (sannolikhet < 10) {
+            blinkPaus = 100
             soundExpression.mysterious.playUntilDone()
         } else if (sannolikhet < 20) {
+            blinkPaus = 50
             soundExpression.yawn.playUntilDone()
         } else if (sannolikhet < 30) {
+            blinkPaus = 25
             soundExpression.sad.playUntilDone()
         }
     }
+    blinkPaus = 10
     basic.pause(1000)
 })
 basic.forever(function () {
     pins.analogWritePin(AnalogPin.P1, triangle(x))
-    x = x + 10
+    x = x + blinkPaus
 })
